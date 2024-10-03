55cea13c4299e7cbcf8c87a5ffe7989878e05a0a
Rohit <frankenstein0671@gmail.com>
2024-10-03 17:37:46 +0530

    Adding Notebook file

--- scripts/utils/config.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-# Setup logging configuration
-def setup_logging():
-    logging.basicConfig(
-        filename='etl_pipeline.log',  # Log file path
-        level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s'
-    )
